8c687d0f8f4add6c82e02846bf80b4035e778e5d
dwarry <dave@warry.org.uk>
2019-08-14 07:06:11 +0100

    Add project files.

---
+++ FileHasher/App.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using System.Windows;
+
+using Microsoft.Win32;
+
+namespace FileHasher
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void CheckCommandLineArgs(object sender, StartupEventArgs e)
+        {
+            if(e.Args.Length == 0)
+            {
+                CheckToggleRegistration();
+                Shutdown(0);
+            }
+            else if(e.Args.Length != 1)
+            {
+                this.Shutdown(1);
+            }
+            else
+            {
+                StartupUri = new Uri("MainWindow.xaml", UriKind.Relative);
+                ShutdownMode = ShutdownMode.OnMainWindowClose;
+            }
+        }
+
+        private const string ShellExtensionRegistryKeyPath = @"Software\Classes\*\shell\FileHasher";
+
+        private void CheckToggleRegistration()
+        {
+            bool alreadyRegistered;
+            using (var regKey = Registry.CurrentUser.OpenSubKey(ShellExtensionRegistryKeyPath))
+            {
+                alreadyRegistered = regKey != null;
+            }
+
+            if (alreadyRegistered)
+            {
+                var response = MessageBox.Show("Do you want to un-register the FileHasher as a Shell Extension?",
+                    "Unregister FileHasher?",
+                    MessageBoxButton.YesNo,
+                    MessageBoxImage.Question,
+                    MessageBoxResult.Yes);
+
+                if (response == MessageBoxResult.Yes)
+                {
+                    Registry.CurrentUser.DeleteSubKeyTree(ShellExtensionRegistryKeyPath);
+                }
+            }
+            else
+            {
+                var response = MessageBox.Show("Do you want to register the FileHasher as a Shell Extension?",
+                    "Register FileHasher?",
+                    MessageBoxButton.YesNo,
+                    MessageBoxImage.Question,
+                    MessageBoxResult.Yes);
+
+                if(response == MessageBoxResult.Yes)
+                {
+                    using (var subkey = Registry.CurrentUser.CreateSubKey(ShellExtensionRegistryKeyPath))
+                    {
+                        subkey.SetValue("", "Calculate Hashes");
+
+                        using(var commandKey = subkey.CreateSubKey("command"))
+                        {
+                            var location = Assembly.GetEntryAssembly().Location;
+                            var command = $"\"{location}\" \"%1\"";
+                            commandKey.SetValue("", command);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ FileHasher/MainWindow.xaml.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FileHasher
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string _filepath;
+        private bool _closed = false;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CloseWindow(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            var args = Environment.GetCommandLineArgs();
+
+            if(args.Length != 2) { this.Close(); }
+
+            _filepath = args[1];
+
+            if (File.Exists(_filepath) && CanReadFile())
+            {
+                Title = "Hashes for " + _filepath;
+                CalculateHashes();
+            }
+            else
+            {
+                Close();
+            }
+        }
+
+        private bool CanReadFile()
+        {
+            var buffer = new byte[4096];
+            using(var stream = File.OpenRead(_filepath))
+            {
+                try
+                {
+                    stream.Read(buffer, 0, buffer.Length);
+                    return true;
+                }
+                catch
+                {
+                    return false;
+                }
+            }
+        }
+
+        private void CalculateHashes()
+        {
+            CalcMd5();
+            CalcSha1();
+            CalcSha256();
+        }
+
+        private Task CalcHash(Func<HashAlgorithm> hashFactory, Action<string> displayResult)
+        {
+            void displayResultIfWindowNotClosed(string s) => Dispatcher.Invoke(() => { if (!_closed) { displayResult(s); } });
+
+            return Task.Run(
+            () =>
+            {
+                displayResultIfWindowNotClosed("Calculating");
+                using(var hash = hashFactory())
+                using (var stream = File.OpenRead(_filepath))
+                {
+                    var hashValue = hash.ComputeHash(stream);
+                    var hashString = BitConverter.ToString(hashValue).Replace("-", String.Empty);
+                    displayResultIfWindowNotClosed(hashString);
+                }
+            } );
+        }
+
+        private void CalcMd5()
+        {
+            CalcHash(MD5.Create, (s) => this.md5.Content = s);
+        }
+
+        private void CalcSha1()
+        {
+            CalcHash(SHA1.Create, (s) => this.sha1.Content = s);
+        }
+
+        private void CalcSha256()
+        {
+            CalcHash(SHA256.Create, (s) => this.sha256.Content = s);
+        }
+
+        private void Window_Closing(object sender, CancelEventArgs e)
+        {
+            _closed = true;
+        }
+    }
+}
